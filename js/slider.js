fe66b7be00a9444c1e53bf788f1f1f1486ec7493
Александр Шарков <SanchiTozzz@yandex.ru>
2020-02-27 23:34:10 +1000

    Личный проект: завершаем строить сетку на флексах

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,27 @@
-// Слайдер
-
+var right = document.querySelector(".right__btn");
+var left = document.querySelector(".left__btn");
+var dot1 = document.querySelector(".dot__1");
+var dot2 = document.querySelector(".dot__2");
+var slides = document.querySelectorAll(".my__slider");
+var dots = document.querySelectorAll(".dot");
 var slideIndex = 1;
+
+right.addEventListener("click", function() {
+  plusSlides(-1);
+})
+
+left.addEventListener("click", function() {
+  plusSlides(1);
+})
+
+dot1.addEventListener("click", function() {
+  currentSlide(1);
+})
+
+dot2.addEventListener("click", function() {
+  currentSlide(2);
+})
+
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -12,62 +33,14 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].classList.remove("slider__block");  
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].classList.remove("active");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].classList.add("slider__block"); 
+  dots[slideIndex-1].classList.add("active");
 }
-
-// var next = document.querySelector(".next");
-// var prev = document.querySelector(".prev");
-// var dot1 = document.querySelector(".dot__1");
-// var dot2 = document.querySelector(".dot__2");
-
-// next.addEventListener ("click", function() {
-//   plusSlides(1);
-// })
-// prev.addEventListener ("click", function() {
-//   plusSlides(-1);
-// })
-// dot1.addEventListener ("click", function() {
-//   currentSlide(1);
-// })
-// dot2.addEventListener ("click", function() {
-//   currentSlide(2);
-// })
-
-// Слайдер преимуществ
-
-var slideServices = 1;
-showServices(slideServices);
-
-function currentServices(n) {
-  showServices(slideServices = n);
-}
-
-function showServices(n) {
-  var a;
-  var services = document.getElementsByClassName("myServices");
-  var dotsServices = document.getElementsByClassName("dotServices");
-  if (n > services.length) {slideServices = 1}    
-  if (n < 1) {slideServices = services.length}
-  for (a = 0; a < services.length; a++) {
-      services[a].style.display = "none";  
-  }
-  for (a = 0; a < dotsServices.length; a++) {
-      dotsServices[a].className = dotsServices[a].className.replace(" active__btn", "");
-  }
-  services[slideServices-1].style.display = "block";  
-  dotsServices[slideServices-1].className += " active__btn";
-}
-
-// ПОПАПЧИКИ
